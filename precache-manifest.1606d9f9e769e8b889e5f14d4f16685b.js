0800e13b1c28670d13a9144ba99e8150aef756cc
Tom Galpin <tomgalpin@Ruths-MacBook-Air.local>
2019-03-30 14:40:29 -0700

    Updates

---
+++ precache-manifest.1606d9f9e769e8b889e5f14d4f16685b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a31bc4089a8532ef48e",
+    "url": "/exercise-react-practice-app/static/css/main.aecb1437.chunk.css"
+  },
+  {
+    "revision": "7a31bc4089a8532ef48e",
+    "url": "/exercise-react-practice-app/static/js/main.7ec2b619.chunk.js"
+  },
+  {
+    "revision": "763e03f65ac4aca39d33",
+    "url": "/exercise-react-practice-app/static/js/runtime~main.39efdcf1.js"
+  },
+  {
+    "revision": "7dc653d0ad7d93ce8cad",
+    "url": "/exercise-react-practice-app/static/js/2.dca4fbf7.chunk.js"
+  },
+  {
+    "revision": "6c289f0d3b075114ff4546cd3961fe20",
+    "url": "/exercise-react-practice-app/index.html"
+  }
+];
